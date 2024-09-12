4e7665c48e97745203f0f28caac8e9471414420b
[tejas parate] <[tejasparate.st@gmail.com]>
2024-09-12 17:53:52 +0530

    added FAQ section

--- script.js
+++ script.js
@@ -124,3 +124,16 @@ document.querySelector('.next').addEventListener('click', function() {
     changeSlide(1);
 });
 
+
+const accordians = document.querySelectorAll('.accordian');
+
+accordians.forEach(accordian => {
+const icon = accordian.querySelector('.icon');
+const answer = accordian.querySelector('.answer');
+
+accordian.addEventListener('click', () => {
+    icon.classList.toggle('active');
+    answer.classList.toggle('active');
+})
+})
+
